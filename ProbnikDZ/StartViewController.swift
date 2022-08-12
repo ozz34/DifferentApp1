b3434b94850b815262badba224c940fd9b55736a
IvanKhudyakov <halex7@yandex.ru>
2022-08-12 16:44:41 +0300

    HW5. Tab Bar  с переходами на экраны

---
+++ ProbnikDZ/StartViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  ProbnikDZ
+//
+//  Created by Иван Худяков on 12.08.2022.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        guard let tabBar = segue.destination as? UITabBarController else {return}
+        guard let viewControllers = tabBar.viewControllers else {return}
+    
+        for viewController in viewControllers {
+            if let firstVC = viewController as? FirstViewController {
+                firstVC.view.backgroundColor = .systemRed
+            } else if let secondVC = viewController as? SecondViewController {
+                secondVC.view.backgroundColor = .systemBlue
+            } else if let navigationVC = viewController as? UINavigationController {
+                guard let thirdVC =  navigationVC.topViewController as? ThirdViewController else {
+                    return
+                }
+                thirdVC.title = "Third Screen"
+                thirdVC.view.backgroundColor = .systemCyan
+                }
+            }
+        }
+    }
+
---
+++ ProbnikDZ/UITAbBarViewController.swift
@@ -0,0 +1,33 @@
+//
+//  UITAbBarViewController.swift
+//  ProbnikDZ
+//
+//  Created by Иван Худяков on 12.08.2022.
+//
+
+import UIKit
+
+class UITAbBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
